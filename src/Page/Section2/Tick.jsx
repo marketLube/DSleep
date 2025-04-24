3ec0735247000b90400f63136b7ce0c5f4bab57c
marketLube <marketlubes@gmail.com>
2025-04-24 09:38:56 +0530

    SL

--- src/Page/Section2/Tick.jsx
+++ src/Page/Section2/Tick.jsx
@@ -6,9 +6,22 @@ import icon3 from "../../assets/logos/dsleep-03.svg";
 import icon4 from "../../assets/logos/dsleep-04.svg";
 import icon5 from "../../assets/logos/dsleep-05.svg";
 import icon6 from "../../assets/logos/dsleep-06.svg";
+import icon7 from "../../assets/logos/mmform.webp";
+import icon8 from "../../assets/logos/kurlon.webp";
 
 const Tick = () => {
-  const logos = [icon1, icon2, icon3, icon6, icon4, icon5];
+  const logos = [icon1, icon2, icon3, icon6, icon4, icon5, icon7, icon8];
+
+  const style = (val) => {
+    let styles = {};
+    if (val === 6) {
+      styles = { maxHeight: "fit-content", width: "8rem" };
+    }
+    if (val === 7) {
+      styles = { maxHeight: "fit-content" };
+    }
+    return styles;
+  };
 
   return (
     <section className="featureList">
@@ -17,6 +30,7 @@ const Tick = () => {
           <img
             src={logo}
             alt="Company logo"
+            style={style(index)}
             className="featureList-item-icon"
           />
         </div>
